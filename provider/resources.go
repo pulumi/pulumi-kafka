f4649651e966ba19366166766296ba1a343d087c
Daniel Bradley <daniel.dbphoto@gmail.com>
2024-05-08 14:22:10 +0000

    Embed version in SDKs
    1. Enable embedding version number in each language.
    2. Configure CI to allow changes to files which include the version number and use new push-based Go SDK publishing.
    3. Remove explicit dependencies (defer to the languages's defaults).

--- provider/resources.go
+++ provider/resources.go
@@ -103,19 +103,12 @@ func Provider() tfbridge.ProviderInfo {
 			"kafka_topic": {Docs: noDocs},
 		},
 		JavaScript: &tfbridge.JavaScriptInfo{
-			Dependencies: map[string]string{
-				"@pulumi/pulumi": "^3.0.0",
-			},
-			DevDependencies: map[string]string{
-				"@types/node": "^10.0.0", // so we can access strongly typed node definitions.
-				"@types/mime": "^2.0.0",
-			},
+			RespectSchemaVersion: true,
 		},
 		Python: (func() *tfbridge.PythonInfo {
 			i := &tfbridge.PythonInfo{
-				Requires: map[string]string{
-					"pulumi": ">=3.0.0,<4.0.0",
-				}}
+				RespectSchemaVersion: true,
+			}
 			i.PyProject.Enabled = true
 			return i
 		})(),
@@ -128,11 +121,10 @@ func Provider() tfbridge.ProviderInfo {
 				mainPkg,
 			),
 			GenerateResourceContainerTypes: true,
+			RespectSchemaVersion:           true,
 		},
 		CSharp: &tfbridge.CSharpInfo{
-			PackageReferences: map[string]string{
-				"Pulumi": "3.*",
-			},
+			RespectSchemaVersion: true,
 			Namespaces: map[string]string{
 				mainPkg: "Kafka",
 			},
